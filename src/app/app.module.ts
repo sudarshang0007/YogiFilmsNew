c08b15856f93b8d8e034c31198fa7ab80cada6f4
sudarshan gaikwad <sudarshangaikwad.it@gmail.com>
2018-04-04 03:27:09 +0530

    Added Angulat 4 test App

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavigatorComponent } from './component/navigator/navigator.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
